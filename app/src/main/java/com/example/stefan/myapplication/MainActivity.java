5ac7879bed58603b952cb98821b543b66d64da81
Stefan <stefan.s27.08@gmail.com>
2018-04-16 08:34:42 +0200

    Start screen

--- app/src/main/java/com/example/stefan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/stefan/myapplication/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.stefan.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button = (Button)findViewById(R.id.btn_display_list);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,DisplayList.class));
+            }
+        });
     }
 }
