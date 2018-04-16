fd064726845bbacafa41e2c77327eb6fb6f2166c
Stefan <stefan.s27.08@gmail.com>
2018-04-16 08:54:36 +0200

    List item model and layout

---
+++ app/src/main/java/com/example/stefan/myapplication/Item.java
@@ -0,0 +1,27 @@
+package com.example.stefan.myapplication;
+
+
+public class Item {
+
+    private String title;
+    private String description;
+    private String imageUrl;
+
+    public Item(String title, String description, String imageUrl) {
+        this.title = title;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
